5787c99a00965cdc718bcfda426caec7563251c3
Victoria L. Flanary <85571685+vlflanary212@users.noreply.github.com>
2024-04-02 14:33:34 -0500

    add current chip-seq code

---
+++ src/chip-seq/ctcf-chip-seq/01_GSE101295_alignment.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+##### SLURM #####
+#SBATCH --job-name=align
+#SBATCH --partition=short
+#SBATCH --time=03:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=128G
+#SBATCH --array=1-3
+#SBATCH --output=%x_%A_%a.out
+#SBATCH --error=%x_%A_%a.err
+
+##### WORKING DIRECTORY #####
+wd="/data/project/sen-lab/Victoria/Neuroblastoma_EDA" 
+
+##### ARRAY #####
+sample_list=$wd"/doc/chip-seq/GSE101295/SRR_Acc_List.txt"
+sample=$(sed -n "$SLURM_ARRAY_TASK_ID"p "$sample_list")
+echo "sample:$sample"
+
+##### VARIABLES #####
+# directories
+dat=$wd/"dat/chip-seq/GSE101295"  #data storage directory
+fastq_dir=$dat/"fastq"  #fastq storage directory
+bam_dir=$dat/"bam"  #bam storage directory
+bigwig_dir=$dat/"bigwig" #bigwig storage directory
+
+# files
+ref_genome="/data/project/sen-lab/genome/hg38/bwa/Homo_sapiens_assembly38.fasta"  #reference genome
+fastq="$fastq_dir/$sample".fastq  #individual fastq files
+
+##### PACKAGES ######
+module load BWA/0.7.17-foss-2018b
+module load SAMtools/1.9-foss-2018b
+module load deepTools/3.3.1-foss-2018b-Python-3.6.6
+
+##### COMMANDS #####
+# map reads and make bam files
+bwa mem -t 10 $ref_genome $fastq | samtools view -S -b -h -F 4 -q 20 > $bam_dir/"$sample"_mapped.bam
+
+# sort by coordinates
+samtools sort -@ 8 -o $bam_dir/"$sample"_sort.bam $bam_dir/"$sample"_mapped.bam
+
+# remove duplicates
+samtools rmdup -s $bam_dir/"sample"_sort.bam $bam_dir/"$sample"_final.bam
+
+# index 
+samtools index $bam_dir/"$sample"_final.bam
+
+# create bigwig file
+bamCoverage -b $bam_dir/"$sample"_final.bam -o $bigwig_dir/"$sample".bw --binSize 200 --normalizeUsing FPKM --effectiveGenomeSize 2913022398 
+
+##### END #####
+echo "done"
---
+++ src/chip-seq/ctcf-chip-seq/02_GSE101295_peak_calling.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##### SLURM #####
+#SBATCH --job-name=peaks
+#SBATCH --partition=express
+#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=128G
+#SBATCH --array=1-3
+#SBATCH --output=%x_%A_%a.out
+#SBATCH --error=%x_%A_%a.err
+
+##### WORKING DIRECTORY #####
+wd="/data/project/sen-lab/Victoria/Neuroblastoma_EDA"
+
+##### ARRAY #####
+sample_list=$wd"/doc/chip-seq/GSE101295/SRR_Acc_List.txt"
+sample=$(sed -n "$SLURM_ARRAY_TASK_ID"p "$sample_list")
+echo "sample:$sample"
+
+##### VARIABLES #####
+# directories
+dat=$wd/"dat/chip-seq/GSE101295"  #data storage directory
+bam_dir=$dat/"bam"  #bam storage directory
+peak_dir=$dat/"peaks"  #peaks storage directory
+
+##### PACKAGES #####
+module load MACS2/2.1.1.20160309-foss-2016a-Python-2.7.11
+
+##### COMMANDS #####
+macs2 callpeak --treatment $bam_dir/"$sample"_final.bam --name "$sample" --outdir $peak_dir --gsize hs --nomodel --shift 0 --extsize 200 --keep-dup all --call-summits --pvalue 0.01 
+
+##### END #####
+echo "done"
